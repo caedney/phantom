c85ec06f298abf38868c7272b35b1478441472cd
Craig Edney <caedney@btinternet.com>
2021-08-30 14:50:33 +0100

    Refactor Dashboard component

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,52 +1,119 @@
 import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import dayjs from "dayjs";
 import styled from "styled-components";
-import { useDispatch } from "react-redux";
-import { csv } from "d3";
 
-import LineChart from "../line-chart/LineChart";
-import Filter from "../filter/Filter";
-import Heading from "../heading/Heading";
-import Paragraph from "../paragraph/Paragraph";
-import { updateData } from "../../reducers/dataReducer";
+import LineChart from "../../components/LineChart/LineChart";
+import breakpoints from "../../utils/breakpoints";
+import dateFormat from "../../utils/dateFormat";
+import {
+  updateLabels,
+  updateNewCases,
+  updateTotalCases,
+} from "../../reducers/dataReducer";
 
-const Stage = styled.div`
-  max-width: 1200px;
-  margin: auto;
-  padding: 60px;
-`;
-
-const Container = styled.div`
+const DashboardContainer = styled.div`
   background-color: #ffffff;
   border-radius: 16px;
-  padding: 48px 60px;
+  padding: 12px 20px 16px 12px;
   border: 1px solid hsl(0, 0%, 80%);
-`;
 
-const Header = styled.div`
-  text-align: center;
+  @media ${breakpoints.laptopL} {
+    padding: 36px 60px 48px 60px;
+  }
 `;
 
 function Dashboard() {
   const dispatch = useDispatch();
+  const { data, labels, newCases, totalCases } = useSelector(
+    state => state.data
+  );
+  const { borough, month, monthOptions } = useSelector(state => state.filter);
+
+  const reduceCases = (acc, cur) => {
+    if (acc.new.length > 12) {
+      const currentDay = Number(cur.date.split("-")[2]) - 1;
+
+      acc.new[currentDay] += Number(cur.new_cases);
+      acc.total[currentDay] += Number(cur.total_cases);
+    } else {
+      const month = Number(cur.date.split("-")[1]) - 1;
+      const endOfMonth = dayjs(cur.date).daysInMonth();
+      const lastDay = `${cur.date.substr(0, 7)}-${endOfMonth}`;
+
+      acc.new[month] += Number(cur.new_cases);
+
+      if (cur.date === lastDay) {
+        acc.total[month] += Number(cur.total_cases);
+      }
+    }
+
+    return acc;
+  };
 
   useEffect(() => {
-    csv("/data/covid-cases.csv").then(data => {
-      // console.log(data);
-      dispatch(updateData(data));
+    let currentLabels = monthOptions.map(({ label }) => label.substr(0, 3));
+
+    if (month) {
+      const daysInMonth = dayjs(month.value).daysInMonth();
+
+      currentLabels = Array.from(Array(daysInMonth), (_, i) =>
+        dateFormat(i + 1)
+      );
+    }
+
+    const caseData =
+      month || borough
+        ? data.filter(({ area_name, date }) => {
+            if (borough && month) {
+              return (
+                borough.value === area_name &&
+                month.value.split("-")[1] === date.split("-")[1]
+              );
+            } else if (borough) {
+              return borough.value === area_name;
+            } else {
+              return month.value.split("-")[1] === date.split("-")[1];
+            }
+          })
+        : data;
+
+    const reducedCases = caseData.reduce(reduceCases, {
+      new: new Array(currentLabels.length).fill(0),
+      total: new Array(currentLabels.length).fill(0),
     });
-  }, [dispatch]);
+
+    dispatch(updateLabels(currentLabels));
+    dispatch(updateNewCases(reducedCases.new));
+    dispatch(updateTotalCases(reducedCases.total));
+  }, [borough, month, dispatch, monthOptions, data]);
+
+  const lineChartData = {
+    labels,
+    datasets: [
+      {
+        label: "# of New Cases",
+        data: newCases,
+        backgroundColor: "rgb(38, 132, 255)",
+        borderColor: "rgba(38, 132, 255, 0.2)",
+        fill: false,
+        tension: 0.4,
+      },
+      {
+        label: "# of Total Cases",
+        data: totalCases,
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgba(255, 99, 132, 0.2)",
+        fill: false,
+        tension: 0.4,
+      },
+    ],
+  };
 
   return (
-    <Stage>
-      <Header>
-        <Heading component="h1">COVID-19</Heading>
-        <Paragraph>The effect of Covid-19 across London Boroughs</Paragraph>
-      </Header>
-      <Filter />
-      <Container>
-        <LineChart />
-      </Container>
-    </Stage>
+    <DashboardContainer>
+      <LineChart data={lineChartData} />
+    </DashboardContainer>
   );
 }
 
