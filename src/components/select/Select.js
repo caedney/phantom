0df259ad8fa52f1f8ad6d63b8cda1580dddb9e6d
Craig Edney <caedney@btinternet.com>
2021-08-30 14:56:38 +0100

    Refactor Select component

--- src/components/select/Select.js
+++ src/components/select/Select.js
@@ -2,40 +2,34 @@ import React from "react";
 import PropTypes from "prop-types";
 import ReactSelect from "react-select";
 
-const dummyOptions = [
-  { value: "chocolate", label: "Chocolate" },
-  { value: "strawberry", label: "Strawberry" },
-  { value: "vanilla", label: "Vanilla" },
-];
-
-const colourStyles = {
+const colorStyles = {
   container: styles => ({ ...styles, flex: 1 }),
   control: styles => ({
     ...styles,
     backgroundColor: "white",
     minHeight: "44px",
   }),
+  singleValue: styles => ({
+    ...styles,
+    overflow: "visible",
+  }),
 };
 
 function Select(props) {
-  const { options, ...rest } = props;
+  const { options, styles, ...rest } = props;
 
   return (
-    <ReactSelect
-      isClearable
-      options={options}
-      styles={colourStyles}
-      {...rest}
-    />
+    <ReactSelect isClearable options={options} styles={styles} {...rest} />
   );
 }
 
 Select.propTypes = {
-  options: PropTypes.arrayOf(PropTypes.object),
+  options: PropTypes.array.isRequired,
+  styles: PropTypes.object,
 };
 
 Select.defaultProps = {
-  options: dummyOptions,
+  styles: colorStyles,
 };
 
 export default Select;
