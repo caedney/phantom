639fddffb74135f27f18551ad9535f2efce9bd3c
Craig Edney <caedney@btinternet.com>
2021-08-30 14:57:00 +0100

    Move chart options to external file

---
+++ src/components/LineChart/chartOptions.js
@@ -0,0 +1,16 @@
+const chartOptions = {
+  plugins: {
+    legend: {
+      labels: {
+        usePointStyle: true,
+      },
+    },
+  },
+  scales: {
+    y: {
+      beginAtZero: true,
+    },
+  },
+};
+
+export default chartOptions;
