2d9c2f37bea51365ac4b405d8a39695118b6ecd1
Craig Edney <caedney@btinternet.com>
2021-08-30 14:55:49 +0100

    Add Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+
+import Heading from "../../components/Heading/Heading";
+import Paragraph from "../../components/Paragraph/Paragraph";
+
+const HeaderContainer = styled.div`
+  text-align: center;
+`;
+
+function Header() {
+  return (
+    <HeaderContainer>
+      <Heading component="h1">COVID-19</Heading>
+      <Paragraph>
+        The effect of Covid-19 across London Boroughs in 2020
+      </Paragraph>
+    </HeaderContainer>
+  );
+}
+
+export default Header;
