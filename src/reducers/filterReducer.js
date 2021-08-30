45ae57c3e4d984f95fccb6f29b16c8aebf862f08
Craig Edney <caedney@btinternet.com>
2021-08-30 14:52:20 +0100

    Add label options to filter reducer

--- src/reducers/filterReducer.js
+++ src/reducers/filterReducer.js
@@ -3,6 +3,21 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   month: null,
   borough: null,
+  monthOptions: [
+    { value: "2020-01", label: "January" },
+    { value: "2020-02", label: "February" },
+    { value: "2020-03", label: "March" },
+    { value: "2020-04", label: "April" },
+    { value: "2020-05", label: "May" },
+    { value: "2020-06", label: "June" },
+    { value: "2020-07", label: "July" },
+    { value: "2020-08", label: "August" },
+    { value: "2020-09", label: "September" },
+    { value: "2020-10", label: "October" },
+    { value: "2020-11", label: "November" },
+    { value: "2020-12", label: "December" },
+  ],
+  boroughOptions: [],
 };
 
 const filterReducer = createSlice({
@@ -12,7 +27,7 @@ const filterReducer = createSlice({
     updateMonth: (state, { payload }) => {
       return {
         ...state,
-        month: payload && payload.value,
+        month: payload,
       };
     },
     updateBorough: (state, { payload }) => {
@@ -21,9 +36,16 @@ const filterReducer = createSlice({
         borough: payload,
       };
     },
+    updateBoroughOptions: (state, { payload }) => {
+      return {
+        ...state,
+        boroughOptions: payload,
+      };
+    },
   },
 });
 
-export const { updateMonth, updateBorough } = filterReducer.actions;
+export const { updateMonth, updateBorough, updateBoroughOptions } =
+  filterReducer.actions;
 
 export default filterReducer.reducer;
